0c91d337ee0cac13891b50b21fabe5780cf4e5d6
steventong <tongwanglin@gmail.com>
2024-04-28 23:54:30 +0800

    update

--- Sources/SynologySwiftKit/BizFlow/DeviceConnection/DeviceConnection.swift
+++ Sources/SynologySwiftKit/BizFlow/DeviceConnection/DeviceConnection.swift
@@ -84,7 +84,7 @@ extension DeviceConnection {
     /**
      当前URL
      */
-    private func getCurrentConnectionUrl() -> (type: ConnectionType, url: String)? {
+    func getCurrentConnectionUrl() -> (type: ConnectionType, url: String)? {
         if let url = UserDefaults.standard.string(forKey: UserDefaultsKeys.DISK_STATION_CONNECTION_URL.keyName) {
             let type = UserDefaults.standard.integer(forKey: UserDefaultsKeys.DISK_STATION_CONNECTION_TYPE.keyName)
             if let typeEnum = ConnectionType(rawValue: type) {
