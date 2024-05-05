9b63067a220d9beae35bea65cc9e417420806808
steventong <tongwanglin@gmail.com>
2024-05-05 16:06:54 +0800

    Update

--- Sources/SynologySwiftKit/BizFlow/BatchQueryData/QueryAllPlaylists.swift
+++ Sources/SynologySwiftKit/BizFlow/BatchQueryData/QueryAllPlaylists.swift
@@ -13,7 +13,7 @@ public class QueryAllPlaylists {
     public init() {
     }
 
-    public func queryAllPlaylists(queryPlaylistSongs: Bool = true, batchSize: Int = 5000, threads: Int = 3,
+    public func queryAllPlaylists(queryPlaylistSongs: Bool = true, batchSize: Int = 5000, maxThreads: Int = 3,
                                   onPlaylistTaskUpdate: @escaping (_ playlists: [Playlist], _ total: Int) -> Void,
                                   onPlaylistSongTaskUpdate: @escaping (_ playlist: Playlist, _ songs: [Song], _ total: Int) -> Void,
                                   onTaskFinish: @escaping () -> Void) {
@@ -28,11 +28,11 @@ public class QueryAllPlaylists {
             }
 
             // 播放列表
-            let playlists = await batchQueryPlaylistList(total: total, batchSize: batchSize, threads: threads, onTaskUpdate: onPlaylistTaskUpdate)
+            let playlists = await batchQueryPlaylistList(total: total, batchSize: batchSize, maxThreads: maxThreads, onTaskUpdate: onPlaylistTaskUpdate)
 
             // 播放列表内的歌曲列表
             if queryPlaylistSongs {
-                await batchQueryPlaylistGetInfo(playlists: playlists, batchSize: batchSize, threads: threads, onTaskUpdate: onPlaylistSongTaskUpdate)
+                await batchQueryPlaylistGetInfo(playlists: playlists, batchSize: batchSize, maxThreads: maxThreads, onTaskUpdate: onPlaylistSongTaskUpdate)
             }
 
             onTaskFinish()
@@ -53,38 +53,40 @@ extension QueryAllPlaylists {
     /**
      batch queryPlaylistList
      */
-    private func batchQueryPlaylistList(total: Int, batchSize: Int, threads: Int, onTaskUpdate: @escaping (_ playlists: [Playlist], _ total: Int) -> Void) async -> [Playlist] {
+    private func batchQueryPlaylistList(total: Int, batchSize: Int, maxThreads: Int, onTaskUpdate: @escaping (_ playlists: [Playlist], _ total: Int) -> Void) async -> [Playlist] {
         // task count
         let taskCount = total / batchSize + 1
         Logger.info("batchQueryPlaylistList task, total count = \(total), taskCount = \(taskCount)")
 
+        let targetThreadsLimit = taskCount > maxThreads ? maxThreads : taskCount
+
         // execute task
-        let playlist = await withTaskGroup(of: [Playlist].self, returning: [Playlist].self, body: { taskGroup in
+        let playlists = await withTaskGroup(of: [Playlist].self, returning: [Playlist].self, body: { taskGroup in
             // 限制并发 https://stackoverflow.com/questions/70976323/how-to-constrain-concurrency-like-maxconcurrentoperationcount-with-swift-con
-            for taskIndex in 0 ..< threads {
+            for taskIndex in 0 ..< targetThreadsLimit {
                 taskGroup.addTask {
                     await self.queryPlaylistList(taskIndex: taskIndex, batchSize: batchSize, total: total, onTaskUpdate: onTaskUpdate)
                 }
             }
 
-            var waitTaskIndex = threads
-            while await taskGroup.next() != nil && waitTaskIndex < taskCount {
-                taskGroup.addTask { [waitTaskIndex] in
-                    await self.queryPlaylistList(taskIndex: waitTaskIndex, batchSize: batchSize, total: total, onTaskUpdate: onTaskUpdate)
-                }
-                waitTaskIndex += 1
-            }
-
+            var waitTaskIndex = targetThreadsLimit
             var playlists: [Playlist] = []
-            for await result in taskGroup {
-                playlists.append(contentsOf: result)
+
+            while let playlist = await taskGroup.next(), playlist != nil {
+                playlists.append(contentsOf: playlist)
+                if waitTaskIndex < taskCount {
+                    taskGroup.addTask { [waitTaskIndex] in
+                        await self.queryPlaylistList(taskIndex: waitTaskIndex, batchSize: batchSize, total: total, onTaskUpdate: onTaskUpdate)
+                    }
+                    waitTaskIndex += 1
+                }
             }
 
             return playlists
         })
 
         Logger.info("batchQueryPlaylistList task, all task done, total count = \(total), taskCount = \(taskCount)")
-        return playlist
+        return playlists
     }
 
     /**
@@ -99,22 +101,24 @@ extension QueryAllPlaylists {
     /**
      batch query music in playlist
      */
-    private func batchQueryPlaylistGetInfo(playlists: [Playlist], batchSize: Int, threads: Int, onTaskUpdate: @escaping (_ playlist: Playlist, _ songs: [Song], _ total: Int) -> Void) async {
+    private func batchQueryPlaylistGetInfo(playlists: [Playlist], batchSize: Int, maxThreads: Int, onTaskUpdate: @escaping (_ playlist: Playlist, _ songs: [Song], _ total: Int) -> Void) async {
         // task count
         let taskCount = playlists.count / batchSize + 1
         Logger.info("batchQueryPlaylistGetInfo task, total count = \(playlists.count), taskCount = \(taskCount)")
 
+        let targetThreadsLimit = taskCount > maxThreads ? maxThreads : taskCount
+
         // execute task
         await withTaskGroup(of: Int.self, body: { taskGroup in
             // 限制并发 https://stackoverflow.com/questions/70976323/how-to-constrain-concurrency-like-maxconcurrentoperationcount-with-swift-con
-            for taskIndex in 0 ..< threads {
+            for taskIndex in 0 ..< targetThreadsLimit {
                 taskGroup.addTask {
                     let playlist = await self.queryPlaylistGetInfo(playlist: playlists[taskIndex], onTaskUpdate: onTaskUpdate)
                     return playlist.count
                 }
             }
 
-            var waitTaskIndex = threads
+            var waitTaskIndex = targetThreadsLimit
             while await taskGroup.next() != nil && waitTaskIndex < taskCount {
                 taskGroup.addTask { [waitTaskIndex] in
                     let playlist = await self.queryPlaylistGetInfo(playlist: playlists[waitTaskIndex], onTaskUpdate: onTaskUpdate)
