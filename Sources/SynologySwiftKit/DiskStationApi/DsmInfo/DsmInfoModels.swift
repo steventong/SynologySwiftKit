faa11e1fe0098068311b31be4ab84942050a756b
steventong <tongwanglin@gmail.com>
2024-05-12 21:53:34 +0800

    update

--- Sources/SynologySwiftKit/DiskStationApi/DsmInfo/DsmInfoModels.swift
+++ Sources/SynologySwiftKit/DiskStationApi/DsmInfo/DsmInfoModels.swift
@@ -10,19 +10,18 @@ import Foundation
 extension DsmInfoApi {
     public class DsmInfo: Decodable {
         /**
-         {"data":{"codepage":"chs","model":"DS920+","ram":20480,"serial":"2080SBRSRX48H","temperature":61,"temperature_warn":false,"time":"Sat Feb  3 10:33:59 2024","uptime":1512862,"version":"69057","version_string":"DSM 7.2.1-69057 Update 3"},"success":true}
-
+         {"data":{"codepage":"chs","model":"","ram":20480,"serial":"","temperature":61,"temperature_warn":false,"time":"Sat Feb  3 10:33:59 2024","uptime":0000,"version":"69057","version_string":"DSM 7.2.1-69057 Update 3"},"success":true}
          */
 
-        var codepage: String?
-        var model: String?
-        var ram: Int?
-        var serial: String?
-        var temperature: Int?
-        var temperature_warn: Bool?
-        var time: String?
-        var uptime: Int?
-        var version: String?
-        var version_string: String?
+        public var codepage: String?
+        public var model: String?
+        public var ram: Int?
+        public var serial: String?
+        public var temperature: Int?
+        public var temperature_warn: Bool?
+        public var time: String?
+        public var uptime: Int?
+        public var version: String?
+        public var version_string: String?
     }
 }
