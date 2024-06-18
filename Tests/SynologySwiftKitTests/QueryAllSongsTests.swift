bcce1346070047bbb46c26c240723a733f958cad
steventong <tongwanglin@gmail.com>
2024-06-19 00:31:53 +0800

    Update

--- Tests/SynologySwiftKitTests/QueryAllSongsTests.swift
+++ Tests/SynologySwiftKitTests/QueryAllSongsTests.swift
@@ -9,6 +9,11 @@ import XCTest
 
 class QueryAllSongsTests: XCTestCase {
     func test() async throws {
+        UserDefaults.standard.setValue(SecretKey.password, forKey: UserDefaultsKeys.DISK_STATION_CONNECTION_URL.keyName)
+        UserDefaults.standard.setValue(ConnectionType.custom_domain.rawValue, forKey: UserDefaultsKeys.DISK_STATION_CONNECTION_TYPE.keyName)
+
+        UserDefaults.standard.setValue(SecretKey.sid, forKey: UserDefaultsKeys.DISK_STATION_AUTH_SESSION_SID.keyName)
+
         let queryAllSongs = QueryAllSongs()
 
         queryAllSongs.queryAllSongs(batchSize: 500, threads: 4, onTaskUpdate: { songs, total in
