fb6baa48f9ba9f1278f5c6c976216f8d3dc0bd8f
steventong <tongwanglin@gmail.com>
2024-06-29 02:40:14 +0800

    Update

--- Sources/SynologySwiftKit/DiskStationApi/AudioStation/ApiList/StreamApi.swift
+++ Sources/SynologySwiftKit/DiskStationApi/AudioStation/ApiList/StreamApi.swift
@@ -9,24 +9,86 @@ import Foundation
 
 extension AudioStationApi {
     /**
-     High: /webapi/AudioStation/stream.cgi/0.wav?api=SYNO.AudioStation.Stream&version=2&method=transcode&format=wav&id=
-     M: /webapi/AudioStation/stream.cgi/0.mp3?api=SYNO.AudioStation.Stream&version=2&method=transcode&format=mp3&id=
-     L: /webapi/AudioStation/stream.cgi/0.mp3?api=SYNO.AudioStation.Stream&version=2&method=transcode&format=mp3&id=
-
-     Original: /webapi/AudioStation/stream.cgi/0.mp3?api=SYNO.AudioStation.Stream&version=2&method=stream&id= ??
-     Original: /webapi/AudioStation/stream.cgi/0.mp3?api=SYNO.AudioStation.Stream&version=2&method=stream&id= ??   format=wav
+     构建音乐播放地址
+     m4a: /webapi/AudioStation/stream.cgi/0.m4a?api=SYNO.AudioStation.Stream&version=2&method=stream&id=music_593
      */
-    public func songStreamUrl(id: String, position: Int = 0, quality: SongStreamQuality) throws -> URL {
+    public func songStreamUrl(id: String, fileExtension: String? = nil, quality: SongStreamQuality) throws -> URL {
+        if fileExtension == "m4a" {
+            return try buildStreamURL(id: id, path: "\(id).m4a", method: "stream")
+        }
+
         let songFileName = "/\(id).\(quality.format)"
         let method = quality == .ORIGINAL ? "stream" : "transcode"
 
-        let api = try DiskStationApi(api: .SYNO_AUDIO_STATION_STREAM, path: songFileName, method: method, version: 2, parameters: [
-            "id": id,
-            "position": position,
-            "format": quality.format,
-            "bitrate": quality.bitrate ?? "",
-        ])
+        return try buildStreamURL(id: id, path: "\(id).m4a", method: "stream", format: quality.format, bitrate: quality.bitrate)
+    }
 
+    /**
+     构造音乐播放地址
+     */
+    public func songStreamUrl(song: Song, quality: SongStreamQuality) throws -> URL {
+        // todo
+        return URL(string: "")!
+    }
+}
+
+extension AudioStationApi {
+    /**
+     构建播放地址
+     */
+    private func buildStreamURL(id: String, path: String, method: String,
+                                format: String? = nil,
+                                bitrate: Int? = nil) throws -> URL {
+        // build parameters
+        var parameters: [String: Any] = ["id": id, "position": 0]
+
+        if let format = format {
+            parameters["format"] = format
+        }
+
+        if let bitrate = bitrate {
+            parameters["bitrate"] = bitrate
+        }
+
+        // build api model
+        let api = try DiskStationApi(api: .SYNO_AUDIO_STATION_STREAM, path: path, method: method, version: 2, parameters: parameters)
+        // assemble stream url
         return try api.assembleRequestUrl()
+    }
+
+    private func isStreamAudio(song: Song, z: Bool) -> Bool {
+        let filePath = song.path.lowercased()
+        let bitrate = song.audio?.bitrate ?? 0
+        let frequency = song.audio?.frequency ?? 0
+
+        if filePath.hasSuffix(".aac") || filePath.hasSuffix(".m4a") || filePath.hasSuffix(".m4b") {
+            return !isBitrateALAC(bitrate: bitrate)
+        }
+
+        if frequency > 48000 {
+            return false
+        }
+
+        if filePath.hasSuffix(".mp3") {
+            return true
+        }
+
+        if !filePath.hasSuffix(".3gp") && !filePath.hasSuffix(".mp4") && !filePath.hasSuffix(".ts") {
+            if filePath.hasSuffix(".flac") {
+                return !z
+            }
+            if filePath.hasSuffix(".ogg") {
+                return true
+            }
+            if !filePath.hasSuffix(".mkv") && filePath.hasSuffix(".wav") {
+                return true
+            }
+        }
+
+        return false
+    }
+
+    private func isBitrateALAC(bitrate: Int) -> Bool {
+        return bitrate > 320000
     }
 }
