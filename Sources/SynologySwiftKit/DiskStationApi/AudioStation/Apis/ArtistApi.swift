a02fbd9009b09438097e64c94e62e67db6e047a1
Steven <4138760+steventong@users.noreply.github.com>
2024-10-06 21:47:53 +0800

    update artist api

--- Sources/SynologySwiftKit/DiskStationApi/AudioStation/Apis/ArtistApi.swift
+++ Sources/SynologySwiftKit/DiskStationApi/AudioStation/Apis/ArtistApi.swift
@@ -8,14 +8,30 @@
 import Foundation
 
 extension AudioStationApi {
-    public func artistList() async throws -> (total: Int, data: [Artist]) {
-        let api = try DiskStationApi(api: .SYNO_AUDIO_STATION_ARTIST, method: "list", version: 1, parameters: [
-            "library": "all",
-            "limit": 5000,
-            "offset": 0,
-            "sort_by": "name",
-            "sort_direction": "asc",
-        ])
+    /**
+     query artist list
+     */
+    public func artistList(limit: Int = 1000, offset: Int = 0,
+                           library: String = "shared",
+                           filter: String? = nil, keyword: String? = nil,
+                           sort_by: String = "name", sort_direction: String = "ASC") async throws -> (total: Int, data: [Artist]) {
+        var parameters: [String: Any] = [
+            "library": library,
+            "limit": limit,
+            "offset": offset,
+            "sort_by": sort_by,
+            "sort_direction": sort_direction,
+        ]
+
+        if let filter {
+            parameters["filter"] = filter
+        }
+
+        if let keyword {
+            parameters["keyword"] = keyword
+        }
+
+        let api = try DiskStationApi(api: .SYNO_AUDIO_STATION_ARTIST, method: "list", version: 4, parameters: parameters)
 
         let result = try await api.requestForData(resultType: ArtistListResult.self)
         return (result.total, result.artists)
