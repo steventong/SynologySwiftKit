15966613ea7fc86c65323e01707cb81270fe6212
Steven <4138760+steventong@users.noreply.github.com>
2024-11-10 00:37:01 +0800

    歌曲文件转化

--- Sources/SynologySwiftKit/DiskStationApi/AudioStation/Apis/StreamApi.swift
+++ Sources/SynologySwiftKit/DiskStationApi/AudioStation/Apis/StreamApi.swift
@@ -12,114 +12,158 @@ extension AudioStationApi {
      构建音乐播放地址
      m4a: /webapi/AudioStation/stream.cgi/0.m4a?api=SYNO.AudioStation.Stream&version=2&method=stream&id=music_593
      */
-    public func songStreamUrl(id: String, fileExtension: String? = nil, quality: SongStreamQuality) throws -> URL {
-        let streamFormat = songStreamFormat(fileExtension: fileExtension ?? "", quality: quality)
-        let streamMethod = songStreamMethod(fileExtension: fileExtension ?? "", quality: quality)
-        let songFileName = "/\(id).\(streamFormat)"
+    public func songStreamUrl(id: String, path: String, bitrate: Int, frequency: Int, quality: SongStreamQuality) throws -> URL {
+        // build parameters
+        var parameters: [String: Any] = ["id": encodeIdForURL(id: id)]
 
-        if streamMethod == "stream" {
-            // 原始格式播放
-            return try buildStreamURL(id: id, path: songFileName, method: streamMethod)
+        // 构建播放地址 getPlayUrl
+        if isToTransCode(id: id, path: path, bitrate: bitrate, frequency: frequency) {
+            if isSupportMP3() && isFormatMp3() {
+                parameters["format"] = "mp3"
+                parameters["bitrate"] = downloadBitrate(quality: quality)
+                parameters["ext"] = ".mp3"
+            } else if isSupportWAV() {
+                parameters["format"] = "wav"
+                parameters["ext"] = ".wav"
+            }
+
+            let api = try DiskStationApi(api: .SYNO_AUDIO_STATION_STREAM, path: path, method: "transcode", version: 1, parameters: parameters)
+            return try api.assembleRequestUrl()
         } else {
-            // 转码播放
-            return try buildStreamURL(id: id, path: songFileName, method: streamMethod, format: streamFormat, bitrate: quality.bitrate)
-        }
-    }
+            parameters["ext"] = getExtFromFilePath(filePath: path)
 
-    /**
-     构造音乐播放地址
-     */
-    public func songStreamUrl(song: Song, quality: SongStreamQuality) throws -> URL {
-        // todo
-        return URL(string: "")!
-    }
-
-    /**
-     文件扩展名
-     */
-    public func songStreamFormat(fileExtension: String, quality: SongStreamQuality) -> String {
-        switch fileExtension.lowercased() {
-        case "m4a":
-            "m4a"
-        case "ogg":
-            "ogg"
-        default:
-            quality.format
-        }
-    }
-
-    /**
-     传输方式
-     */
-    public func songStreamMethod(fileExtension: String, quality: SongStreamQuality) -> String {
-        switch fileExtension.lowercased() {
-        case "m4a":
-            "stream"
-        case "ogg":
-            "stream"
-        default:
-            quality == .ORIGINAL ? "stream" : "transcode"
+            let api = try DiskStationApi(api: .SYNO_AUDIO_STATION_STREAM, path: path, method: "stream", version: 1, parameters: parameters)
+            return try api.assembleRequestUrl()
         }
     }
 }
 
 extension AudioStationApi {
     /**
-     构建播放地址
+     是否转码播放
      */
-    private func buildStreamURL(id: String, path: String, method: String,
-                                format: String? = nil,
-                                bitrate: Int? = nil) throws -> URL {
-        // build parameters
-        var parameters: [String: Any] = ["id": id, "position": 0]
+    private func isToTransCode(id: String, path: String, bitrate: Int, frequency: Int) -> Bool {
+        let isStreamPlaying = isStreamAudio(path: path, bitrate: bitrate, frequency: frequency)
 
-        if let format = format {
-            parameters["format"] = format
+        // 基本的机器都支持转码
+        if isSupportTranscoding() {
+            if isStreamPlaying == true {
+                return true
+            }
         }
 
-        if let bitrate = bitrate {
-            parameters["bitrate"] = bitrate
-        }
-
-        // build api model
-        let api = try DiskStationApi(api: .SYNO_AUDIO_STATION_STREAM, path: path, method: method, version: 2, parameters: parameters)
-        // assemble stream url
-        return try api.assembleRequestUrl()
-    }
-
-    private func isStreamAudio(song: Song, z: Bool) -> Bool {
-        let filePath = song.path.lowercased()
-        let bitrate = song.audio?.bitrate ?? 0
-        let frequency = song.audio?.frequency ?? 0
-
-        if filePath.hasSuffix(".aac") || filePath.hasSuffix(".m4a") || filePath.hasSuffix(".m4b") {
-            return !isBitrateALAC(bitrate: bitrate)
-        }
-
-        if frequency > 48000 {
-            return false
-        }
-
-        if filePath.hasSuffix(".mp3") {
+        if id.hasPrefix("music_v") || id.hasPrefix("music_p_v") {
             return true
-        }
-
-        if !filePath.hasSuffix(".3gp") && !filePath.hasSuffix(".mp4") && !filePath.hasSuffix(".ts") {
-            if filePath.hasSuffix(".flac") {
-                return !z
-            }
-            if filePath.hasSuffix(".ogg") {
-                return true
-            }
-            if !filePath.hasSuffix(".mkv") && filePath.hasSuffix(".wav") {
-                return true
-            }
         }
 
         return false
     }
 
-    private func isBitrateALAC(bitrate: Int) -> Bool {
-        return bitrate > 320000
+    /**
+
+     */
+    private func downloadBitrate(quality: SongStreamQuality) -> Int {
+        switch quality {
+        case .ORIGINAL:
+            return 128000
+        case .HIGH:
+            return 320000
+        case .MEDIUM:
+            return 192000
+        case .LOW:
+            return 128000
+        }
+    }
+
+    /**
+     是否支持转码播放
+     */
+    private func isSupportTranscoding() -> Bool {
+        // 基本的机器都支持转码，应该从nas的接口获取 transcode_capability
+        true
+    }
+
+    /**
+     是否支持直接传音频流？
+     */
+    private func isStreamAudio(path: String, bitrate: Int, frequency: Int) -> Bool {
+        if frequency > 48000 {
+            return false
+        }
+
+        let name = path.lowercased()
+
+        if name.hasSuffix(".mp3") {
+            return true
+        }
+
+        if name.hasSuffix(".3gp") || name.hasSuffix(".mp4") {
+            return false
+        }
+
+        if name.hasSuffix(".m4a") {
+            return bitrate <= 320000
+        }
+
+        if name.hasSuffix(".m4b") {
+            return true
+        }
+
+        if name.hasSuffix(".aac") {
+            return true
+        }
+
+        if name.hasSuffix(".flac") {
+            return true
+        }
+        if name.hasSuffix(".ogg") {
+            return true
+        }
+        if name.hasSuffix(".mkv") {
+            return false
+        }
+        if name.hasSuffix(".wav") {
+            return true
+        }
+
+        return false
+    }
+
+    /**
+     encodeIdForURL
+     */
+    private func encodeIdForURL(id: String) -> String {
+        return id.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? id
+    }
+
+    /**
+     nas接口返回是否支持mp3转码
+     */
+    private func isSupportMP3() -> Bool {
+        true
+    }
+
+    /**
+     nas接口返回是否支持wav转码
+     */
+    private func isSupportWAV() -> Bool {
+        true
+    }
+
+    /**
+     if mp3 file
+     */
+    private func isFormatMp3() -> Bool {
+        // 转码设置？
+        return true
+    }
+
+    /** 获取文件的扩展名**/
+    private func getExtFromFilePath(filePath: String) -> String {
+        guard let pos = filePath.lastIndex(of: ".") else {
+            return "."
+        }
+        let ext = filePath[filePath.index(after: pos)...]
+        return "." + String(ext)
     }
 }
