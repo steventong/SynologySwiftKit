65b1414630bf523413aa3d41cf1ac345f71ec0d7
Steven <4138760+steventong@users.noreply.github.com>
2025-02-08 14:20:55 +0800

    测试URL

--- Sources/SynologySwiftKit/DiskStationApi/AudioStation/Apis/StreamApi.swift
+++ Sources/SynologySwiftKit/DiskStationApi/AudioStation/Apis/StreamApi.swift
@@ -12,7 +12,6 @@ extension AudioStationApi {
      构建音乐播放地址
      m4a: /webapi/AudioStation/stream.cgi/0.m4a?api=SYNO.AudioStation.Stream&version=2&method=stream&id=music_593
      <p>DS audio 支持以下格式（其余格式无法播放）：AAC、AIF、AIFF、M4A、M4B、MP3、WAV、APE、FLAC、Ogg Vorbis、WMA、WMA VBR、WMA PRO。</p>
-     <p>DS audio 支持以下格式（其余格式无法播放）：AAC、AIF、AIFF、M4A、M4B、MP3、WAV、APE、FLAC、Ogg Vorbis、WMA、WMA VBR、WMA PRO。</p>
 
      https://kb.synology.cn/zh-cn/DSM/tutorial/prevent_DS_audio_from_automatically_converting_audio_files
 
@@ -71,37 +70,102 @@ extension AudioStationApi {
 
         // 构建播放地址 getPlayUrl
         // 当前的音频是否需要转码
-        let isNeedToTransCode = isNeedToTransCode(id: id, path: path, bitrate: bitrate, frequency: frequency)
-        // 用户选择原始音质（串流），且支持串流播放
-        if quality == .ORIGINAL && !isNeedToTransCode {
-            parameters["format"] = fileExtension
-
-            let api = try DiskStationApi(api: .SYNO_AUDIO_STATION_STREAM, path: "/0.\(fileExtension)", method: "stream", version: 1, parameters: parameters)
-            return try api.assembleRequestUrl()
+        let streamMethod = getAudioStreamForceMethod(id: id, path: path, bitrate: bitrate, frequency: frequency)
+        if streamMethod == .STREAM {
+            // must use stream
+            return try buildStreamUrl(fileExtension: fileExtension, parameters: &parameters)
+        } else if streamMethod == .TRANSCODE {
+            // must by transcode
+            return try buildTranscodeUrl(fileExtension: fileExtension, quality: quality, parameters: &parameters)
+        } else if quality == .ORIGINAL {
+            // user choose use original (stream)
+            return try buildStreamUrl(fileExtension: fileExtension, parameters: &parameters)
         } else {
-            parameters["format"] = "mp3"
-            parameters["bitrate"] = getTransCodeBitrate(quality: quality)
-           
-            let api = try DiskStationApi(api: .SYNO_AUDIO_STATION_STREAM, path: "/0.mp3", method: "transcode", version: 1, parameters: parameters)
-            return try api.assembleRequestUrl()
+            // user choose transcode
+            return try buildTranscodeUrl(fileExtension: fileExtension, quality: quality, parameters: &parameters)
         }
     }
 }
 
 extension AudioStationApi {
     /**
-     是否进行转码播放
-     */
-    private func isNeedToTransCode(id: String, path: String, bitrate: Int, frequency: Int) -> Bool {
+      播放模式
+
+      AVPlayer（iOS 原生播放器）支持播放的音视频格式主要取决于 iOS 系统的解码能力 和 AVFoundation 框架的支持。以下是官方支持的格式：
+      音频格式（Audio Formats）
+
+     AVPlayer 支持的主要音频格式包括：
+         •    AAC（Advanced Audio Codec）：.m4a、.mp3、.mp4
+         •    MP3：.mp3
+         •    Apple Lossless (ALAC)：.m4a
+         •    FLAC（iOS 11+）：.flac
+         •    AIFF（Audio Interchange File Format）：.aiff、.aif
+         •    WAV：.wav
+
+     不支持（需要转码）： APE、WMA、OGG 等。
+      */
+    private func getAudioStreamForceMethod(id: String, path: String, bitrate: Int, frequency: Int) -> StreamMethodEnum? {
         // 整轨音频文件不支持stream，强制转码
         if id.hasPrefix("music_v") || id.hasPrefix("music_p_v") {
             Logger.info("整轨音频文件不支持stream，强制转码, id: \(id)")
-            return true
+            return .TRANSCODE
         }
 
-        // 看看是串流（stream）还是转码（transcode）
-        let isSupportStreamPlaying = isSupportStreamPlaying(path: path, bitrate: bitrate, frequency: frequency)
-        return !isSupportStreamPlaying
+        // 根据文件类型来判断
+        if frequency > 48000 {
+            Logger.info("采样率大于48k，使用转码，path: \(path), frequency: \(frequency)")
+            return .TRANSCODE
+        }
+
+        let _path = path.lowercased()
+
+        if _path.hasSuffix(".mp3") {
+            return .TRANSCODE
+        }
+
+//        if _path.hasSuffix(".3gp") || _path.hasSuffix(".mp4") {
+//            Logger.info("特定格式，使用转码，path: \(path), .3gp/.mp4")
+//            return .STREAM
+//        }
+//
+//        if _path.hasSuffix(".m4a") && bitrate > 320000 {
+//            Logger.info("特定格式，比特率大于320k，使用转码，path: \(path), .m4a，bitrate: \(bitrate)")
+//            return false
+//        }
+
+        if _path.hasSuffix(".m4a") {
+            Logger.info("特定格式，使用串流，path: \(path), .m4a")
+            return .STREAM
+        }
+
+//
+//        if _path.hasSuffix(".m4b") {
+//            return true
+//        }
+//
+//        if _path.hasSuffix(".aac") {
+//            return true
+//        }
+//
+//        if _path.hasSuffix(".flac") {
+//            return true
+//        }
+//
+//        if _path.hasSuffix(".ogg") {
+//            return true
+//        }
+//
+//        if _path.hasSuffix(".mkv") {
+//            return false
+//        }
+//
+//        if _path.hasSuffix(".wav") {
+//            return true
+//        }
+//
+//        Logger.info("没有匹配到各种条件，默认使用转码")
+//        return false
+        return nil
     }
 
     /**
@@ -130,59 +194,6 @@ extension AudioStationApi {
     }
 
     /**
-     是否支持直接传音频流？支持的就不需要转码
-     */
-    private func isSupportStreamPlaying(path: String, bitrate: Int, frequency: Int) -> Bool {
-        if frequency > 48000 {
-            Logger.info("采样率大于48k，使用转码，path: \(path), frequency: \(frequency)")
-            return false
-        }
-
-        let name = path.lowercased()
-
-        if name.hasSuffix(".mp3") {
-            return true
-        }
-
-        if name.hasSuffix(".3gp") || name.hasSuffix(".mp4") {
-            Logger.info("特定格式，使用转码，path: \(path), .3gp/.mp4")
-            return false
-        }
-
-        if name.hasSuffix(".m4a") && bitrate > 320000 {
-            Logger.info("特定格式，比特率大于320k，使用转码，path: \(path), .m4a，bitrate: \(bitrate)")
-            return false
-        }
-
-        if name.hasSuffix(".m4b") {
-            return true
-        }
-
-        if name.hasSuffix(".aac") {
-            return true
-        }
-
-        if name.hasSuffix(".flac") {
-            return true
-        }
-
-        if name.hasSuffix(".ogg") {
-            return true
-        }
-
-        if name.hasSuffix(".mkv") {
-            return false
-        }
-
-        if name.hasSuffix(".wav") {
-            return true
-        }
-
-        Logger.info("没有匹配到各种条件，默认使用转码")
-        return false
-    }
-
-    /**
      if mp3 file
      */
     private func isFormatMp3() -> Bool {
@@ -197,5 +208,26 @@ extension AudioStationApi {
         }
         let ext = filePath[filePath.index(after: pos)...]
         return "." + String(ext)
+    }
+
+    /**
+     buildStreamUrl
+     */
+    private func buildStreamUrl(fileExtension: String, parameters: inout [String: Any]) throws -> URL {
+        parameters["format"] = fileExtension
+
+        let api = try DiskStationApi(api: .SYNO_AUDIO_STATION_STREAM, path: "/0.\(fileExtension)", method: "stream", version: 1, parameters: parameters)
+        return try api.assembleRequestUrl()
+    }
+
+    /**
+     buildTranscodeUrl
+     */
+    private func buildTranscodeUrl(fileExtension: String, quality: SongStreamQuality, parameters: inout [String: Any]) throws -> URL {
+        parameters["format"] = "mp3"
+        parameters["bitrate"] = getTransCodeBitrate(quality: quality)
+
+        let api = try DiskStationApi(api: .SYNO_AUDIO_STATION_STREAM, path: "/0.mp3", method: "transcode", version: 1, parameters: parameters)
+        return try api.assembleRequestUrl()
     }
 }
---
+++ Sources/SynologySwiftKit/DiskStationApi/AudioStation/Models/StreamApi+Models.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  SynologySwiftKit
+//
+//  Created by Steven on 2025/2/8.
+//
+
+import Foundation
+
+
+public enum StreamMethodEnum {
+    case STREAM
+    case TRANSCODE
+}
