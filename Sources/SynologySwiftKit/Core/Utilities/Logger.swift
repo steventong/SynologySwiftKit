96cd900dbab5e247fb74bb56e055f2a86d9943da
Steven <tongwanglin@gmail.com>
2026-05-20 08:15:31 +0800

    feat: add SynologySwiftKit tag to all logger output

--- Sources/SynologySwiftKit/Core/Utilities/Logger.swift
+++ Sources/SynologySwiftKit/Core/Utilities/Logger.swift
@@ -10,6 +10,7 @@ import OSLog
 /// Backed by `OSLog`, controlled by the `isEnabled` switch (enabled by default).
 public final class Logger {
     private static let osLog = OSLog(subsystem: "me.itwl.SynologySwiftKit", category: "SynologySwiftKit")
+    private static let kitTag = "[SynologySwiftKit]"
     public static var isEnabled = true
 
     private init() {}
@@ -43,6 +44,6 @@ public final class Logger {
     private static func log(_ message: String, type: OSLogType, filePath: String, fileNumber: Int) {
         guard isEnabled else { return }
         let swiftFileName = (filePath as NSString).lastPathComponent
-        os_log("[%{public}@:%{public}d] %{public}@", log: osLog, type: type, swiftFileName, fileNumber, message)
+        os_log("%{public}@ [%{public}@:%{public}d] %{public}@", log: osLog, type: type, kitTag, swiftFileName, fileNumber, message)
     }
 }
